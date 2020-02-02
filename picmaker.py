44b865432c920ca146b2937dc9dc14b9e6dc1429
aditihaiman <ahaiman00@stuy.edu>
2020-02-02 15:46:11 -0500

    complete

--- picmaker.py
+++ picmaker.py
@@ -1,3 +1,5 @@
+import math
+
 
 f = open("image.ppm", "w")
 
@@ -5,8 +7,24 @@ f.write("P3\n500 500\n255\n")
 
 for y in range(500):
     for x in range(500):
-        num = str(x%255)
-        f.write(num + " " + num + " " + num + " ")
+#        num = str(x%100)
+#        num2 = str(y%255)
+        num = str(int(math.fabs(math.ldexp(x,y)*255)))
+        num2 = str(int(math.fabs(math.sin(x*y))*255))
+        num3 = str(int(math.fabs(math.cos(x/(y+1)))*255))
+        #print(num, num2)
+        if(y<83):
+            f.write(num2 + " " + num3 + " " + num + " ")
+        elif(y<167):
+            f.write(num2 + " " + num + " " + num3 + " ")
+        elif(y<250):
+            f.write(num + " " + num2 + " " + num3 + " ")
+        elif(y<333):
+            f.write(num + " " + num3 + " " + num2 + " ")
+        elif(y<417):
+            f.write(num3 + " " + num + " " + num2 + " ")
+        else:
+            f.write(num3 + " " + num2 + " " + num + " ")
     f.write("\n")
 
 f.close()
